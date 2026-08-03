d39b1027d619b536b841609b3fb0972059c6f4bb
vimalp <vimalp@gmail.com>
2026-08-02 20:28:31 -0700

    Rename sqline studio project
    Change project name to vThrottle

---
+++ sls_project/vThrottle_events.py
@@ -0,0 +1,60 @@
+
+def powerOff(event_struct):
+    return
+
+
+def setTurnOut(event_struct):
+    return
+
+
+def setLocoSpeed(event_struct):
+    return
+
+
+def setThrottleLoco(event_struct):
+    return
+
+
+def setLocoReverse(event_struct):
+    return
+
+
+def setLocoHorn(event_struct):
+    return
+
+
+def openLocoFunc(event_struct):
+    return
+
+
+def setLocoFunc0(event_struct):
+    return
+
+
+def setLocoFunc3(event_struct):
+    return
+
+
+def setLocoFunc7(event_struct):
+    return
+
+
+def setLocoFunc11(event_struct):
+    return
+
+
+def setLocoFunc8(event_struct):
+    return
+
+
+def setLocoBlk(event_struct):
+    return
+
+
+def setRouteState(event_struct):
+    return
+
+
+def getKeyboardText(event_struct):
+    return
+
